9d22b8ee1aee7d370080c4a5c1bda728f669b30b
nino <nino@haeberlen-online.de>
2019-01-03 21:39:42 +0100

    MainActivity.java: - implemented callback method that is called when permission was granted

--- app/src/main/java/com/haeberlen/nino/privatetrainingprogress/MainActivity.java
+++ app/src/main/java/com/haeberlen/nino/privatetrainingprogress/MainActivity.java
@@ -2,10 +2,11 @@ package com.haeberlen.nino.privatetrainingprogress;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,23 +23,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (ContextCompat.checkSelfPermission(MainActivity.this,
+        if (ActivityCompat.checkSelfPermission(this,
                 Manifest.permission.READ_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED) {
 
             // Permission is not granted so request it
-            ActivityCompat.requestPermissions(MainActivity.this,
+            ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
                     MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
         } else {
-
             // Permission already granted
-        }
-
-        if (ContextCompat.checkSelfPermission(MainActivity.this,
-                Manifest.permission.READ_EXTERNAL_STORAGE)
-                == PackageManager.PERMISSION_GRANTED) {
-            //  beim ersten start, wenn berechtigung angefragt wird, kommt das programm hier nicht her
 
             // Example of a call to a native method
             TextView tv = (TextView) findViewById(R.id.sample_text);
@@ -51,4 +45,26 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+    /* *
+     * we come into this function once the asynchronous method requestPermissions has returned
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        Log.d("PrivateTrainingProgress", "onRequestPermissionsResult()");
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission granted
+                    TextView tv = (TextView) findViewById(R.id.sample_text);
+                    tv.setText(stringFromJNI());
+                } /*else {
+                    // Permission denied
+                }*/
+                break;
+            }
+        }
+    }
 }
